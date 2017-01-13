0fd38df8986acbcbdf8570abde047b9324f27ddc
Junko Tsuji <jnktsj@gmail.com>
2017-01-13 01:29:49 -0500

    Optimized and made it really parallel k-means

--- opt_kmeans.R
+++ opt_kmeans.R
@@ -8,6 +8,8 @@
 library(flexclust)
 library(parallel)
 
+# make it reproducible 
+set.seed(144)
 
 opt_kmeans <- function(data_file, kRange=2:30, distFun=distCor, centFun=centMean,
                        name_list=NA, thread_num=16){
@@ -20,38 +22,35 @@ opt_kmeans <- function(data_file, kRange=2:30, distFun=distCor, centFun=centMean
         data <- data[target_id,]
     }
 
-    # parallelization
-    cl <- FALSE
-    if(thread_num > 1){
-        cl <- makeCluster(thread_num, type="PSOCK")
-        clusterCall(cl, function() require("flexclust"))
+    # function for different ks
+    kmFam <- kccaFamily(dist=distFun, cent=centFun)
+    FUN <- function(k_i){
+        res_i <- kcca(data, k_i, family=kmFam)
+        list(k=res_i@k, centers=res_i@centers, cluster=res_i@cluster,
+             totaldist=res_i@totaldist, distsum=info(res_i, "distsum"),
+             cldist=res_i@cldist)
     }
 
-    # do clustering: repeat 3 times for each k (taken from the default setting)
-    res <- stepFlexclust(data, k=kRange, nrep=3, verbose=FALSE, multicore=cl, drop=TRUE,
-                         FUN=function(x, k, group=NULL, simple=FALSE, save.data=FALSE)
-                             kcca(x, k, family=kccaFamily(dist=distFun, cent=centFun)))
+    # parallelization and run
+    cl <- makeCluster(thread_num, type="PSOCK")
+    clusterCall(cl, function() require("flexclust"))
+    res <- parLapply(cl, kRange, FUN)
 
+    # get total distance
+    unsorted_k <- unlist(lapply(res, function(x) x$k))
+    unsorted_distsum <- unlist(lapply(res, function(x) x$distsum))
+    totaldist <- mean(unique(unlist(lapply(res, function(x) x$totaldist))))
 
-    # get total distance, compute AIC and BIC
-    k <- c(1, res@k)
-    dist_sum <- rep(0, length(kRange))
-    aic <- rep(0, length(kRange))
-    bic <- rep(0, length(kRange))
-    for(i in 1:length(kRange)){
-        ds <- info(res@models[[i]], "distsum")
-        dist_sum[i] <- ds
-        aic[i] <- ds + 2 * ncol(data) * res@k[i]
-        bic[i] <- ds + log(nrow(data)) * ncol(data) * res@k[i]
-    }
+    index <- sort(unsorted_k, index.return=TRUE)
+    k <- c(1, index$x)
+    dist_sum <-c(totaldist, unsorted_distsum[index$ix])
 
-    # add values when k = 1
-    dist_sum <- c(res@totaldist, dist_sum)
-    aic <- c(res@totaldist + 2 * ncol(data), aic)
-    bic <- c(res@totaldist + log(nrow(data)) * ncol(data), bic)
+    # compute R squared, AIC, BIC
+    rsq <- 1 - (dist_sum*(nrow(data)-1)) / (dist_sum[1]*(nrow(data)-k))    
+    aic <- dist_sum + 2 * ncol(data) * k
+    bic <- dist_sum + log(nrow(data)) * ncol(data) * k
 
     # R squared values: estimate optimal k
-    rsq <- 1 - (dist_sum*(nrow(data)-1)) / (dist_sum[1]*(nrow(data)-k))
     vrsq <- (rsq-min(rsq)) / (max(rsq)-min(rsq))
     vk <- (k - min(k)) / (max(k)-min(k))
     dsq <- (vk)^2 + (vrsq - 1)^2
@@ -79,16 +78,17 @@ opt_kmeans <- function(data_file, kRange=2:30, distFun=distCor, centFun=centMean
     dev.off()
 
     # output clustering results: labels and centers
-    opt_k <- unique(rsq_opt_k, aic_opt_k, bic_opt_k)
+    opt_k <- unique(c(rsq_opt_k, aic_opt_k, bic_opt_k))
+    print(opt_k)
     for(opt_k_i in opt_k){
-        i <- which(res@k == opt_k_i)
+        i <- which(unsorted_k == opt_k_i)
 
         label_file <- paste(prefix,"_k",opt_k_i,"_label.txt", sep="")
-        out_data <- cbind(res@models[[i]]@cluster, res@models[[i]]@cldist[,1])
+        out_data <- cbind(res[[i]]$cluster, res[[i]]$cldist[,1])
         write.table(out_data, label_file, quote=FALSE, sep="\t", row.names=TRUE, col.names=FALSE)
 
         center_file <- paste(prefix,"_k",opt_k_i,"_center.txt", sep="")
-        out_data <- res@models[[i]]@centers
+        out_data <- res[[i]]$centers
         rownames(out_data) <- 1:opt_k_i
         write.table(out_data, center_file, quote=FALSE, sep="\t", row.names=TRUE, col.names=FALSE)
     }
